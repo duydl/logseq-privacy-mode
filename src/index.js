4b57f4c67947f16de114f0ab1eff6b651d6abfd6
duydl <duy.dole.00ece@gmail.com>
2023-10-10 17:35:00 +0900

    fix: localized workflow

--- src/index.js
+++ src/index.js
@@ -144,7 +144,7 @@ const main = () => {
   logseq.App.onMacroRendererSlotted(({ slot, payload }) => {
     const [type, scheme] = payload.arguments
     if (type == "privacymode_encrypt") {
-      if (logseq.settings.encrypt == true) {
+      if (logseq.settings.toggle_encrypt_decrypt == "Encrypt") {
       
         return logseq.provideUI({
           key: type + payload.uuid,
@@ -156,7 +156,7 @@ const main = () => {
           data-block-uuid="${payload.uuid}"
           ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 17a2 2 0 0 0 2-2a2 2 0 0 0-2-2a2 2 0 0 0-2 2a2 2 0 0 0 2 2m6-9a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V10a2 2 0 0 1 2-2h1V6a5 5 0 0 1 5-5a5 5 0 0 1 5 5v2h1m-6-5a3 3 0 0 0-3 3v2h6V6a3 3 0 0 0-3-3Z"/></svg></a>`,
         })}
-        else {
+        else if (logseq.settings.toggle_encrypt_decrypt == "Decrypt") {
           return logseq.provideUI({
             key: type + payload.uuid,
             slot, 
@@ -248,7 +248,7 @@ const main = () => {
   logseq.App.onMacroRendererSlotted(({ slot, payload }) => {
     const [type, scheme] = payload.arguments
     if (type == "privacymode_hide") {
-      if (logseq.settings.hide == true) {
+      if (logseq.settings.toggle_hide_show == "Hide") {
       
         return logseq.provideUI({
           key: type + payload.uuid,
@@ -260,7 +260,7 @@ const main = () => {
           data-block-uuid="${payload.uuid}"
           ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M8.073 12.194L4.212 8.333c-1.52 1.657-2.096 3.317-2.106 3.351L2 12l.105.316C2.127 12.383 4.421 19 12.054 19c.929 0 1.775-.102 2.552-.273l-2.746-2.746a3.987 3.987 0 0 1-3.787-3.787zM12.054 5c-1.855 0-3.375.404-4.642.998L3.707 2.293L2.293 3.707l18 18l1.414-1.414l-3.298-3.298c2.638-1.953 3.579-4.637 3.593-4.679l.105-.316l-.105-.316C21.98 11.617 19.687 5 12.054 5zm1.906 7.546c.187-.677.028-1.439-.492-1.96s-1.283-.679-1.96-.492L10 8.586A3.955 3.955 0 0 1 12.054 8c2.206 0 4 1.794 4 4a3.94 3.94 0 0 1-.587 2.053l-1.507-1.507z"/></svg></a>`,
         })}
-        else {
+        else if (logseq.settings.toggle_hide_show == "Show"){
           return logseq.provideUI({
             key: type + payload.uuid,
             slot, 
