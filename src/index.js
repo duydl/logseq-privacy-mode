52b163f39bb260aa70160baf4c2e080399ba1f29
duydl <duy.dole.00ece@gmail.com>
2023-10-10 15:12:56 +0900

    fix: complete UI

--- src/index.js
+++ src/index.js
@@ -5,40 +5,55 @@ import CryptoJS from "crypto-js";
 const parser = new DOMParser();
 
 const settings = [
-    {
+  {
+    key: "encrypt_options",
+    title: "Options for encrypted blocks",
+    type: "heading",
+    default: null
+  },
+      {
         key: "encrypt",
-        title: "Preferred name for your private block",
+        description: "Encrypt/Decrypt",
         type: "boolean",
         default: true
-    },
-    {
-      key: "hide",
-      title: "Preferred name for your private block",
-      type: "boolean",
-      default: true
-    },
-    {
-      key: "idToadd",
-      title: "Preferred name for your private block",
-      type: "string",
-      default: "private"
-  },
+      },
+      {
+        key: "encrypt_tag",
+        title: "Tag for your encrypted block",
+        type: "string",
+        default: ""
+      },
+      // {
+      //   key: "encrypt_tag",
+      //   title: "Password for encryption.",
+      //   description: "Warning: Changing password would break your content when decrypt. ",
+      //   type: "string",
+      //   default: ""
+      // },
   {
-    key: "styleOfBlock0",
-    title: "Preferred style for your private block",
-    type: "string",
-    default: `#private {
-      background-color: var(--ls-primary-text-color);
-     }`
+    key: "hide_options",
+    title: "Options for hidden blocks",
+    type: "heading",
+    default: null
   },
-  {
-    key: "styleOfBlock1",
-    title: "Preferred style for your private block",
-    type: "string",
-    default: `#private {
-      background-color: var(--color-level-1);
-     }`
-  },
+      {
+        key: "hide",
+        description: "Hide/Show",
+        type: "boolean",
+        default: true
+      },
+      {
+        key: "hide_tag",
+        title: "Tag for your hidden block",
+        type: "string",
+        default: ""
+      },
+      {
+        key: "hidden_style",
+        title: "Preferred style for your hidden block",
+        type: "string",
+        default: `background-color: black; color: black`
+      },
   
 ];
 
@@ -46,55 +61,39 @@ const settings = [
 
 const main = () => {
 
-
-  // == Encrypt Block == //
-
-  const query = ` [:find (pull ?h [*])
-    :in $ 
-    :where
-    [?p :block/name "private"]
-    [?h :block/refs ?p]] `
-    
-    // `[:find (pull ?p[*]) 
-    // :in $ 
-    // :where [?pin :block/name "mypage"] 
-    //   [?b :block/refs ?pin] 
-    //   [?b :block/page ?p]]`
-  
-  logseq.DB.datascriptQuery(query).then(result => console.log(result))
+  logseq.useSettingsSchema(settings);
 
   console.log('=== logseq-privacy-mode Plugin Loaded ===')
-  logseq.useSettingsSchema(settings);
-  logseq.App.queryElementById("app-container").then(result => console.log(result))
-
-  // toolbar icon
+  
   logseq.App.registerUIItem("toolbar", {
-      key: "logseg-encrypt",
+      key: "logseg-privacy-mode-settings",
       template:
-          `<a data-on-click="change_encrypting_mode" class="button">
+          `<a data-on-click="show_settings" class="button">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M11.1 15h1.8q.225 0 .388-.188t.112-.412l-.475-2.625q.5-.25.788-.725T14 10q0-.825-.588-1.413T12 8q-.825 0-1.413.588T10 10q0 .575.288 1.05t.787.725L10.6 14.4q-.05.225.113.413T11.1 15Zm.9 6.9q-.175 0-.325-.025t-.3-.075Q8 20.675 6 17.637T4 11.1V6.375q0-.625.363-1.125t.937-.725l6-2.25q.35-.125.7-.125t.7.125l6 2.25q.575.225.938.725T20 6.375V11.1q0 3.5-2 6.538T12.625 21.8q-.15.05-.3.075T12 21.9Z"/></svg>
           </a>`
   });
 
   logseq.provideModel({
-    change_encrypting_mode(e) {
-
-      if (logseq.settings.encrypt) {
-        logseq.updateSettings({"encrypt": false,})
-        logseq.UI.showMsg("Encrypt False")
-      }
-      else {
-        logseq.updateSettings({"encrypt": true,})
-        logseq.UI.showMsg("Encrypt True")
-      }
+    show_settings(e) {
+      logseq.showSettingsUI()
+      // if (logseq.settings.encrypt) {
+      //   logseq.updateSettings({"encrypt": false,})
+      //   logseq.UI.showMsg("Encrypt False")
+      // }
+      // else {
+      //   logseq.updateSettings({"encrypt": true,})
+      //   logseq.UI.showMsg("Encrypt True")
+      // }
     }
   });
+
+  // == Encrypt Block == //
 
   logseq.App.registerUIItem("toolbar", {
     key: "logseg-mass-decrypt",
     template:
         `<a data-on-click="mass_decrypt" class="button">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M16 20V10H4v10h12m0-12c1.1 0 2 .9 2 2v10c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V10c0-1.1.9-2 2-2h9V6c0-1.7-1.3-3-3-3S7 4.3 7 6H5c0-2.8 2.2-5 5-5s5 2.2 5 5v2h1m-6 9c-1.1 0-2-.9-2-2s.9-2 2-2s2 .9 2 2s-.9 2-2 2M22 7h-2v6h2V7m0 8h-2v2h2v-2Z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M11.991 0a.883.883 0 0 0-.871.817v3.02a.883.883 0 0 0 .88.884a.883.883 0 0 0 .88-.88V.816A.883.883 0 0 0 11.991 0zm7.705 3.109a.88.88 0 0 0-.521.174L16.8 5.231a.88.88 0 0 0 .559 1.563a.88.88 0 0 0 .56-.2l2.37-1.951a.88.88 0 0 0-.594-1.534zM4.32 3.122a.883.883 0 0 0-.611 1.52l2.37 1.951a.876.876 0 0 0 .56.2v-.002a.88.88 0 0 0 .56-1.56L4.828 3.283a.883.883 0 0 0-.508-.16zm7.66 3.228a5.046 5.046 0 0 0-5.026 5.045v1.488H5.787a.967.967 0 0 0-.965.964v9.189a.967.967 0 0 0 .965.964h12.426a.967.967 0 0 0 .964-.964v-9.19a.967.967 0 0 0-.964-.963h-1.168v-1.488A5.046 5.046 0 0 0 11.98 6.35zm.012 2.893a2.152 2.152 0 0 1 2.16 2.152v1.488H9.847v-1.488a2.152 2.152 0 0 1 2.145-2.152zm7.382.503a.883.883 0 1 0 .07 1.763h3.027a.883.883 0 0 0 0-1.76h-3.027a.883.883 0 0 0-.07-.003zM1.529 9.75a.883.883 0 0 0 0 1.76h2.999a.883.883 0 0 0 0-1.76zm10.46 6.774a1.28 1.28 0 0 1 .64 2.393v1.245a.63.63 0 0 1-1.259 0v-1.245a1.28 1.28 0 0 1 .619-2.393z"/></svg>
         </a>`
   });
 
@@ -104,7 +103,7 @@ const main = () => {
     const query = ` [:find (pull ?h [*])
     :in $ 
     :where
-    [?p :block/name "private"]
+    [?p :block/name "${logseq.settings?.encrypt_tag}"]
     [?h :block/refs ?p]] `
   
     const result = await logseq.DB.datascriptQuery(query) 
@@ -113,14 +112,14 @@ const main = () => {
       console.log(result[item])
       decrypt(result[item][0].uuid)
     }
-}});
+  }});
 
 
   logseq.Editor.registerSlashCommand(
     'Add Private Block: Encrypt',
     async () => {
       const { content, uuid } = await logseq.Editor.getCurrentBlock()
-      logseq.Editor.updateBlock(uuid, `{{renderer privacymode_encrypt, default}} #private `)
+      logseq.Editor.updateBlock(uuid, `{{renderer privacymode_encrypt, default}} #${logseq.settings.encrypt_tag} `)
     },
   )
 
@@ -171,41 +170,45 @@ const main = () => {
 
 // == Hide Block == //
 
+
+  logseq.provideStyle({
+    style: `
+    #${logseq.settings.hide_tag}: {${logseq.settings.hidden_style};}
+    `
+  })
+  console.log(`#${logseq.settings.hide_tag}: {${logseq.settings.hidden_style}}`)
+
   logseq.App.registerUIItem("toolbar", {
-      key: "logseg-privacy-block",
+      key: "logseg-show-all",
       template:
-          `<a data-on-click="change_styles_private_block" class="button">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M11.83 9L15 12.16V12a3 3 0 0 0-3-3h-.17m-4.3.8l1.55 1.55c-.05.21-.08.42-.08.65a3 3 0 0 0 3 3c.22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53a5 5 0 0 1-5-5c0-.79.2-1.53.53-2.2M2 4.27l2.28 2.28l.45.45C3.08 8.3 1.78 10 1 12c1.73 4.39 6 7.5 11 7.5c1.55 0 3.03-.3 4.38-.84l.43.42L19.73 22L21 20.73L3.27 3M12 7a5 5 0 0 1 5 5c0 .64-.13 1.26-.36 1.82l2.93 2.93c1.5-1.25 2.7-2.89 3.43-4.75c-1.73-4.39-6-7.5-11-7.5c-1.4 0-2.74.25-4 .7l2.17 2.15C10.74 7.13 11.35 7 12 7Z"/></svg>
+          `<a data-on-click="show_all" class="button">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M20.8 17v-1.5c0-1.4-1.4-2.5-2.8-2.5s-2.8 1.1-2.8 2.5V17c-.6 0-1.2.6-1.2 1.2v3.5c0 .7.6 1.3 1.2 1.3h5.5c.7 0 1.3-.6 1.3-1.2v-3.5c0-.7-.6-1.3-1.2-1.3m-1.3 0h-3v-1.5c0-.8.7-1.3 1.5-1.3s1.5.5 1.5 1.3V17M15 12c-.9.7-1.5 1.6-1.7 2.7c-.4.2-.8.3-1.3.3c-1.7 0-3-1.3-3-3s1.3-3 3-3s3 1.3 3 3m-3 7.5c-5 0-9.3-3.1-11-7.5c1.7-4.4 6-7.5 11-7.5s9.3 3.1 11 7.5c-.2.5-.5 1-.7 1.5C21.5 12 19.8 11 18 11c-.4 0-.7.1-1.1.1C16.5 8.8 14.5 7 12 7c-2.8 0-5 2.2-5 5s2.2 5 5 5h.3c-.2.4-.3.8-.3 1.2v1.3Z"/></svg>
           </a>`
   });
 
   logseq.provideModel({
-    change_styles_private_block(e) {
+    async show_all(e) {
 
-      if (theCurrentStyle == 1) {
-        theCurrentStyle = 0
-        logseq.provideStyle({
-          style: `
-          ${logseq.settings?.styleOfBlock0}
-          `,
-        })
-      } else {
-        theCurrentStyle = 1
-        logseq.provideStyle({
-          style: `
-          ${logseq.settings?.styleOfBlock1}
-          `,
-        })
-      }
-    },
-  });
+    const query = ` [:find (pull ?h [*])
+    :in $ 
+    :where
+    [?p :block/name "${logseq.settings?.hide_tag}"]
+    [?h :block/refs ?p]] `
+  
+    const result = await logseq.DB.datascriptQuery(query) 
+    console.log(result)
+    for (const item in result) {
+      console.log(result[item])
+      show(result[item][0].uuid)
+    }
+  }});
 
 
   logseq.Editor.registerSlashCommand(
     'Add Private Block: Hide',
     async () => {
       const { content, uuid } = await logseq.Editor.getCurrentBlock()
-      logseq.Editor.updateBlock(uuid, `<div id="${logseq.settings?.idToadd}">${content}</div> {{renderer privacymode_hide, ${logseq.settings?.idToadd}}}  #${logseq.settings?.idToadd}`)
+      logseq.Editor.updateBlock(uuid, `<div id="${logseq.settings?.hide_tag}">${content}</div> {{renderer privacymode_hide, default}}  #${logseq.settings?.hide_tag}`)
     },
   )
 
@@ -219,10 +222,9 @@ const main = () => {
           slot, 
           template: `<a
           data-on-click="hide_private_block"
-          data-id-toadd="${logseq.settings?.idToadd}"
+          data-hide-tag="${logseq.settings?.hide_tag}"
           data-slot-id="${slot}"
           data-block-uuid="${payload.uuid}"
-          data-scheme-id=${scheme}"
           ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M8.073 12.194L4.212 8.333c-1.52 1.657-2.096 3.317-2.106 3.351L2 12l.105.316C2.127 12.383 4.421 19 12.054 19c.929 0 1.775-.102 2.552-.273l-2.746-2.746a3.987 3.987 0 0 1-3.787-3.787zM12.054 5c-1.855 0-3.375.404-4.642.998L3.707 2.293L2.293 3.707l18 18l1.414-1.414l-3.298-3.298c2.638-1.953 3.579-4.637 3.593-4.679l.105-.316l-.105-.316C21.98 11.617 19.687 5 12.054 5zm1.906 7.546c.187-.677.028-1.439-.492-1.96s-1.283-.679-1.96-.492L10 8.586A3.955 3.955 0 0 1 12.054 8c2.206 0 4 1.794 4 4a3.94 3.94 0 0 1-.587 2.053l-1.507-1.507z"/></svg></a>`,
         })}
         else {
@@ -231,7 +233,7 @@ const main = () => {
             slot, 
             template: `<a
             data-on-click="show_private_block"
-            data-id-toadd="${logseq.settings?.idToadd}"
+            data-hide-tag="${logseq.settings?.hide_tag}"
             data-slot-id="${slot}"
             data-block-uuid="${payload.uuid}"
             ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 9a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3m0 8a5 5 0 0 1-5-5a5 5 0 0 1 5-5a5 5 0 0 1 5 5a5 5 0 0 1-5 5m0-12.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5Z"/></svg></a>`,
@@ -242,7 +244,7 @@ const main = () => {
 
   logseq.provideModel({
     async hide_private_block(e) {
-      hide(e.dataset.blockUuid, e.dataset.schemeId)  
+      hide(e.dataset.blockUuid, e.dataset.hideTag)  
     },
   });
 
@@ -273,7 +275,9 @@ async function encrypt (blockUuid){
       let [properties, content] = procContent(childElement.content)
 
       if (content) {
-        logseq.Editor.updateBlock(childElement.uuid, CryptoJS.AES.encrypt(content, "password").toString())
+        // logseq.Editor.updateBlock(childElement.uuid, CryptoJS.AES.encrypt(content, "password").toString())
+
+        logseq.Editor.updateBlock(childElement.uuid, btoa(content))
 
         Object.entries(properties).forEach(([key, value]) => {
           logseq.Editor.upsertBlockProperty((childElement.uuid), key, value)
@@ -298,7 +302,9 @@ async function decrypt (blockUuid){
       let [properties, content] = procContent(childElement.content)
 
       if (content) {
-        logseq.Editor.updateBlock(childElement.uuid, CryptoJS.AES.decrypt(content, "password").toString(CryptoJS.enc.Utf8))
+        // logseq.Editor.updateBlock(childElement.uuid, CryptoJS.AES.decrypt(content, "password").toString(CryptoJS.enc.Utf8))
+
+        logseq.Editor.updateBlock(childElement.uuid, atob(content, "password"))
 
         Object.entries(properties).forEach(([key, value]) => {
           logseq.Editor.upsertBlockProperty((childElement.uuid), key, value)
@@ -311,7 +317,7 @@ async function decrypt (blockUuid){
 }
 
 
-async function hide (blockUuid, scheme){
+async function hide (blockUuid, divId){
   const blocks = await logseq.Editor.getBlock(blockUuid, {
     includeChildren: true
   })
@@ -323,7 +329,7 @@ async function hide (blockUuid, scheme){
       let [properties, content] = procContent(childElement.content)
 
       if (content) {
-        logseq.Editor.updateBlock(childElement.uuid, `<div id="${scheme}">${content}</div>`)
+        logseq.Editor.updateBlock(childElement.uuid, `<div id="${divId}">${content}</div>`)
 
         Object.entries(properties).forEach(([key, value]) => {
           logseq.Editor.upsertBlockProperty((childElement.uuid), key, value)
